324e484f2aeb053c1be4c50a2c0e85068eaadb77
root <wtghnh@gmail.com>
2018-03-11 10:54:30 +0000

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "sum.h"
+
+using namespace std;
+
+int main() {
+	int a, b, c = 0;
+	cout << "a : ";
+	cin >> a;
+	cout << "b : ";
+	cin >> b;
+
+	if (a>b) c = sum(a)-sum(b)+b;
+	else if (a<b) c = sum(b)-sum(a)+a;
+	else c = 0;
+
+	cout << c << endl;
+}
